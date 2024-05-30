25a401772e4dba95ab4df6dc032004ade7bfef31
Fausto <Faustobenzo1@gmail.com>
2024-05-30 17:30:07 -0300

    correccion
    correcion de metodo mal escrito

--- src/INTERFACES/IOperacionesBasicas.java
+++ src/INTERFACES/IOperacionesBasicas.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 public interface IOperacionesBasicas <E>{
 
     void agregar(E elemento);
-    void elimiar(E elemento);
+    void eliminar(E elemento);
     void mostrar(E elemento);
     int size();
 
